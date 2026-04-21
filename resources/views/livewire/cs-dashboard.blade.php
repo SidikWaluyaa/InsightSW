3d7c7fb1b002b53cf7ea4c31013075c11acbfff5
SidikWaluyaa <149220571+SidikWaluyaa@users.noreply.github.com>
2026-04-21 11:45:04 +0700

    Reverted CS dashboard labels to Greeting and Konsultasi

--- resources/views/livewire/cs-dashboard.blade.php
+++ resources/views/livewire/cs-dashboard.blade.php
@@ -125,8 +125,8 @@
 
             @php
                 $chatItems = [
-                    ['label' => 'Baru Sapa', 'val' => $sleekflowMetrics['totalGreeting'] ?? 0, 'icon' => 'M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z', 'sub' => 'Masih di Awal'],
-                    ['label' => 'Sedang Konsul', 'val' => $sleekflowMetrics['totalKonsul'] ?? 0, 'icon' => 'M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z', 'sub' => 'Proses Chat'],
+                    ['label' => 'Greeting (Halo)', 'val' => $sleekflowMetrics['totalGreeting'] ?? 0, 'icon' => 'M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z', 'sub' => 'Masih di Awal'],
+                    ['label' => 'Konsultasi', 'val' => $sleekflowMetrics['totalKonsul'] ?? 0, 'icon' => 'M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z', 'sub' => 'Proses Chat'],
                     ['label' => 'Deal (Closing)', 'val' => $sleekflowMetrics['totalClosing'] ?? 0, 'icon' => 'M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z', 'sub' => 'Gol / Selesai'],
                 ];
                 $chatClosingRate = (($sleekflowMetrics['totalKonsul'] ?? 0) + ($sleekflowMetrics['totalClosing'] ?? 0)) > 0 
